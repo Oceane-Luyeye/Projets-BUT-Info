8430a6b8a50e4787895274e6358fc1e62744ed0f
Luyeye Océane <99192949+Oceane-Luyeye@users.noreply.github.com>
2023-05-04 23:51:38 +0200

    Delete Semestre 3 directory

--- Semestre 3/Content/js/scrollToTop.js
+++
@@ -1,6 +0,0 @@
-//Réalisé par Ony
-document.addEventListener('DOMContentLoaded', function() {
-    window.onscroll = function() {
-      document.getElementById("scrollToTop").className = (window.pageYOffset > 100) ? "scrollUpVisible" : "scrollUpInvisible";
-    };
-  });
